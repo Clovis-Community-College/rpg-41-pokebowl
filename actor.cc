2aebfd362a65d54505b0eb3d69a22d8b2b1fd648
ho_1041841 <1041841@my.scccd.edu>
2026-04-08 00:38:49 -0700

    feat: populate basic impl of Actor class

--- actor.cc
+++ actor.cc
@@ -1,2 +1,17 @@
 #include "actor.h"
 #include "llbridges.h"
+
+Actor::Actor(string init_name, XY init_xy, HP init_hp = HP_MAX) 
+	: _name(init_name), _pos(init_xy), _hp(init_hp) {}
+
+string Actor::name() const { return _name; }	
+
+XY Actor::pos() const { return _pos; }
+
+HP Actor::hp() const { return _hp; }
+
+// move() TBI by subclass
+
+// Default behaviour: no HP damage
+// its for the walls
+void Actor::take_damage(HP delta) {}

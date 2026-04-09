ce9e4a6b41dcb953cd935a57231fd363ee9ccabc
ho_1041841 <1041841@my.scccd.edu>
2026-04-09 14:24:12 -0700

    fix: clarify cmt in llbridges

--- llbridges.h
+++ llbridges.h
@@ -11,11 +11,14 @@ class HasInitiative {
 	Feel free to add-edit-delete to manage speed as needed, just don't touch the
 	speed() interface
 	*/
+protected:
+	// this is for Actor instances, ala actor._speed
+	// this is their FINAL speed after calculation through calling speed()
 	int8_t _speed;
 public:
 	// Initial speed:
 	// actor->start_speed() holds the initial speed valur for each actor
-	// use that then add on the dice
+	// use it, then add on the dice onto '_speed'
 	decltype(_speed) speed(Actor* actor);
 };
 

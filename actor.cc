7ce41762990f48695bf1cd327bf48bc62b460fef
ho_1041841 <1041841@my.scccd.edu>
2026-04-09 12:11:46 -0700

    feat, chore: Aleph (appearing name tbd), cstor hell, clean-ups, ???

--- actor.cc
+++ actor.cc
@@ -1,41 +1,76 @@
 #include "actor.h"
 #include "llbridges.h"
 
-Actor::Actor(string init_name, XY init_xy, HP init_hp = HP_MAX) 
-	: _name(init_name), _pos(init_xy), _hp(init_hp) {}
+// Actor
+Actor::Actor(string init_name, XY init_xy, HP init_hp)
+	: _name(init_name), _pos(init_xy), _hp(init_hp), _hp_max(init_hp) {}
+// HP max is init to current HP (aka all actors has 100% health)
 
-string Actor::name() const { return _name; }	
-
+string Actor::name() const { return _name; }
 XY Actor::pos() const { return _pos; }
-
 HP Actor::hp() const { return _hp; }
 
 void Actor::name(string _name_) { _name = _name_; }
 void Actor::pos(XY _pos_) { _pos = _pos_; }
-void Actor::hp(HP _hp_) { _hp = _hp_; }
+void Actor::hp(HP _hp_) {
+	if (_hp_ > HP_MAX)
+		_hp = _hp_max; // cap HP
 
-// move() TBI by subclass
-// defauly: 1 step by direction for hero, 2 steps for monster?????
-// so it has a "cause" for pure virtual
-void Wall::move(Direction d) {}
+	// -1 acts as a DEAD status code. potentially buggy.
+	// can also use 0 for DEAD signal, if death procressing requires 2 frames
+	else if (_hp_ < 0)
+		_hp = -1;
 
-void Hero::move(Direction d) {}
+	else
+		_hp = _hp_;
+}
 
-void Monster::move(Direction d) {}
-
-// Default behaviour: HP damage
-void Actor::take_damage(HP delta) { 
+void Actor::take_damage(HP delta) {
 	decltype(hp()) new_hp = hp() - delta;
 	hp(new_hp);
 }
 
+// Wall
 Wall::Wall(XY xy) : Actor("wall", xy, HP_MAX) {}
 
+void Wall::move(Direction d) {}
+
 void Wall::take_damage(HP delta) {
-	decltype(hp()) new_hp = hp() - 0; // 0 means no mod
+	decltype(hp()) new_hp = hp() - 0;
 	hp(new_hp);
 }
 
+// Hero
+void Hero::move(Direction d) {}
+
+// H subs
+Aleph::Aleph(string _name_, XY _pos_) : Hero(_name_, _pos_, 500) {} // high HP
+Bet::Bet(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+Gimel::Gimel(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+Dalet::Dalet(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+He::He(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+Vav::Vav(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+Zayin::Zayin(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+Chet::Chet(string _name_, XY _pos_) : Hero(_name_, _pos_, 250) {}
+
+void Aleph::take_damage(HP delta) {
+	decltype(hp()) new_hp = hp() - delta/2; // half dam
+	hp(new_hp);
+}
+
+// Monster
+void Monster::move(Direction d) {}
+
 bool Monster::is_boss() const { return false; }
 
+// M sub
 bool Foxtrot::is_boss() const { return true; }
+
+Alpha::Alpha(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Bravo::Bravo(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Charlie::Charlie(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Delta::Delta(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Echo::Echo(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Foxtrot::Foxtrot(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Golf::Golf(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
+Hotel::Hotel(string _name_, XY _pos_) : Monster(_name_, _pos_, 250) {}
--- actor.h
+++ actor.h
@@ -1,10 +1,10 @@
 #ifndef ACTOR_H
 #define ACTOR_H
 
-#include <functional>
-#include <string>
-#include <iostream>
 #include "llbridges.h"
+#include <functional>
+#include <iostream>
+#include <string>
 
 using std::string, std::cout, std::cin, std::function;
 using HP = int32_t;
@@ -16,8 +16,14 @@ struct XY {
 };
 
 enum Direction {
-	LEFT, RIGHT, UP, DOWN,
-	LEFT_UP, RIGHT_UP, LEFT_DOWN, RIGHT_DOWN
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	LEFT_UP,
+	RIGHT_UP,
+	LEFT_DOWN,
+	RIGHT_DOWN
 	// TBD: combination, varying steps per press
 };
 
@@ -30,9 +36,11 @@ private:
 	XY _pos;
 
 	// HP of actor. Should default to HP_MAX.
-	// Invariant: 0 <= hp <= HP_MAX, no negative hp
-	// unless processing the score itself
+	// Invariant: 0 <= hp <= HP_MAX, no negative hp (except for -1, DEAD actor)
 	HP _hp;
+
+	const HP _hp_max; // yes it is UNinitialized, oh the horror. but its deifned
+					  // at cstor so its okay
 
 protected:
 	constexpr static int32_t HP_MAX = INT32_MAX;
@@ -44,13 +52,12 @@ protected:
 
 public:
 	// Cstor
-	Actor(string init_name);
 	Actor(string init_name, XY init_xy, HP init_hp);
 
 	// Get (no set)
 	string name() const;
 	XY pos() const;
-	HP hp() const; 
+	HP hp() const;
 
 	// Move behaviour. TBI by subclasses.
 	// Actor should only move on int32_teger-based steps
@@ -62,6 +69,8 @@ public:
 	virtual void take_damage(HP delta);
 };
 
+// tbd: add overlayable (like sand or water, that has effect)
+
 class Wall : public Actor {
 public:
 	Wall(XY xy);
@@ -70,65 +79,108 @@ public:
 };
 
 class Hero : public Actor, public HasInitiative {
-public:
 	using Actor::Actor;
-	void move(Direction d) override; // !!!!!!!! each hero is unique, so cannot final here
+
+public:
+	void move(Direction d)
+		override; // !!!!!!!! each hero is unique, so cannot final here
 };
 
 // Hero - Hebrew
 // internal names only
 class Aleph : public Hero {
-	using Hero::Hero;
+// Fighter 1, half damage instead of full
+public:
+	Aleph(string _name_, XY _pos_);
+	void take_damage(HP delta) override;
 };
 
 class Bet : public Hero {
-	using Hero::Hero;
+public:
+	Bet(string _name_, XY _pos_);
 };
 
 class Gimel : public Hero {
-	using Hero::Hero;
+public:
+	Gimel(string _name_, XY _pos_);
 };
 
 class Dalet : public Hero {
-	using Hero::Hero;
+public:
+	Dalet(string _name_, XY _pos_);
 };
 
 class He : public Hero {
-	using Hero::Hero;
+public:
+	He(string _name_, XY _pos_);
 };
 
 class Vav : public Hero {
-	using Hero::Hero;
+public:
+	Vav(string _name_, XY _pos_);
 };
 
 class Zayin : public Hero {
-	using Hero::Hero;
+public:
+	Zayin(string _name_, XY _pos_);
 };
 
 class Chet : public Hero {
-	using Hero::Hero;
+public:
+	Chet(string _name_, XY _pos_);
 };
 
 class Monster : public Actor, public HasInitiative {
-public:
 	using Actor::Actor;
+
+public:
 	virtual bool is_boss() const; // default to false
-	void move(Direction d) override; // !!!!!!!! each hero is unique, so cannot final here
+	void move(Direction d)
+		override; // !!!!!!!! each hero is unique, so cannot final here
 };
 
 // Monster - Military
 // internal names only
-class Alpha : public Monster { using Monster::Monster; };
-class Bravo : public Monster { using Monster::Monster; };
-class Charlie : public Monster { using Monster::Monster; };
-class Delta : public Monster { using Monster::Monster; };
-class Echo : public Monster { using Monster::Monster; };
-class Foxtrot : public Monster {
-	using Monster::Monster;
-	bool is_boss() const override; 
+class Alpha : public Monster {
+public:
+	Alpha(string _name_, XY _pos_);
 };
-class Golf : public Monster { using Monster::Monster; };
-class Hotel : public Monster { using Monster::Monster; };
+
+class Bravo : public Monster {
+public:
+	Bravo(string _name_, XY _pos_);
+};
+
+class Charlie : public Monster {
+public:
+	Charlie(string _name_, XY _pos_);
+};
+
+class Delta : public Monster {
+public:
+	Delta(string _name_, XY _pos_);
+};
+
+class Echo : public Monster {
+public:
+	Echo(string _name_, XY _pos_);
+};
+
+class Foxtrot : public Monster {
+public:
+	Foxtrot(string _name_, XY _pos_);
+	bool is_boss() const override;
+};
+
+class Golf : public Monster {
+public:
+	Golf(string _name_, XY _pos_);
+};
+
+class Hotel : public Monster {
+public:
+	Hotel(string _name_, XY _pos_);
+};
 
 // TODO: diagram with links to code snippets
 

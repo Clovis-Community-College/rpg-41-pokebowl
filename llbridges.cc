9fbdab0b362b6692e670ca6dd579bf0f256dcd47
laura_1002948 <1002948@my.scccd.edu>
2026-04-08 19:05:07 -0700

    Speed() in progress

--- llbridges.cc
+++ llbridges.cc
@@ -1,2 +1,9 @@
 #include "actor.h"
 #include "llbridges.h"
+
+int8_t get_speed() {return _speed;};
+
+decltype(_speed) speed() {
+		
+	return speedVal;
+};
--- llbridges.h
+++ llbridges.h
@@ -10,8 +10,10 @@ class HasInitiative {
 	Feel free to add-edit-delete to manage speed as needed, just don't touch the
 	speed() interface
 	*/
-	int8_t _speed;
+	int8_t _speed{};
 public:
+	int8_t get_speed();
 	decltype(_speed) speed();
 	// delclare here & impl in cc sth like: void speed(Actor* actor);
+	void speed(Actor* actor);
 };

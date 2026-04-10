9e98a43c243e63a4e0825f695570ec7fd690d045
laura_1002948 <1002948@my.scccd.edu>
2026-04-09 17:50:17 -0700

    seeded rand() for d20 rolls

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include "actor.h"
+#include <ctime> //to seed d20 rolls in speed(). see llbridges.cc
 
 int main() { 
+	srand(time({}));
 	Wall w({0,0});
 	Aleph h("hero", {0,1});
 	Alpha m("monster", {0,2});

49039ce06f2f95438b03d2918c7365445f2dbf45
?? <??>
2026-04-11 02:10:50 -0700

    cycle based weather system - wip

--- weather.cc
+++ weather.cc
@@ -3,8 +3,42 @@
 #include "actor.h"
 #include <cstdlib>
 
-using namepsace std;
+using namespace std;
 
+// Weather System using a timer based approach, rand similar to real games
+// In-game cycle rand?
 WeatherSystem::WeatherSystem(){
-	weaather = "Clear;
+	weather = "Clear";
+	cycle = 10;
+}
+
+void WeatherSystem::Update(const Map&, const XY& pos){
+	cycle--;
+	// rand this
+
+	if(cycle > 0){
+		return;
+	}
+	
+	int r = rand() %4;
+	//testing approach, wip for a better method for weather
+
+	if(r==0){
+		weather = "Clear";
+	}
+	else if(r == 1){
+		weather = "Rain";
+	}
+	else if(r == 2){
+		weather = "Fog";
+	}
+	else if(r == 3){
+		weather ="Windy";
+	}
+
+	cycle =10;
+}
+
+String WeatherSystem::getWeather() const{
+	return weather;
 }
--- weather.h
+++ weather.h
@@ -4,14 +4,20 @@
 #include <string>
 using namespace std;
 
+//declare others
+class Map;
+struct XY;
+
 class WeatherSystem{
 	private:
 		string weather;
+		int cycle;
 
 	public: 
 		WeatherSystem();
 
-		void setWeather(string w);
+		void Update(const Map& world, const XY& pos);
+
 		string getWeather() const;
 
 };

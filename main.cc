67b58ee0f72b534b573d35c894076ecb53e7928f
McKay Seamons <84572650+MckaySea@users.noreply.github.com>
2026-04-11 17:22:47 -0700

    clarification

--- main.cc
+++ main.cc
@@ -86,7 +86,7 @@ int main() {
           h1.move(dir);
         }
       } else {
-        // Here you could add logic to load the next map!
+        // Here you could add logic to load the next map or whatever we wanna do
       }
     }
 

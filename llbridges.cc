20fbf97cd3e4d3680a60496ae7f1f440128e166e
laura_1002948 <1002948@my.scccd.edu>
2026-04-13 12:14:07 -0700

    testing sort methods for circular linked list

--- llbridges.cc
+++ llbridges.cc
@@ -1,6 +1,7 @@
 #include "actor.h"
 #include <cstdlib>
 #include "llbridges.h"
+#include "circularll.h"
 
 using namespace std;
 
@@ -8,12 +9,26 @@ void HasInitiative::begin_combat(vector<Actor*> involved) {
 	vector<Actor*> heroL;
 	vector<Actor*> enemyL;
 	for (auto x : involved) {
+		speed(x);
 		if (x->type() == "hero") {
 			heroL.push_back(x);
 		} else if (x->type() == "monster") {
 			enemyL.push_back(x);
 		}
+		/*
+		while (heroL.size() or enemyL.size()) {
+			if (heroL.size()) {
+				for (int c = 0; c < heroL.size(); c++) {
+					for (int d = 0; d < heroL.size(); d++) {	
+					}
+				}
+			}
+			if (enemyL.size()) {
+				
+			}
+		}*/
 	}
+	CLL linkeddList;
 };
 
 void HasInitiative::check_speed() {

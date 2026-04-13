a2eaf155dcdc683e2ef59da0c3c383db0bef2bdd
laura_1002948 <1002948@my.scccd.edu>
2026-04-12 21:52:14 -0700

    Size updates on insert

--- circularll.cc
+++ circularll.cc
@@ -5,12 +5,14 @@ using namespace bridges;
 void CLL::list_insert(Actor* a) {
 	if (!head) {
 		head = tail = new Node{a};
+		size = 1;
 		return;
 	}
 	if (head == tail || head != tail) {
 		tail->next = new Node{a};
 		tail = tail->next;
 		tail->next = head;
+		size++;
 		return;
 	}
 	else {

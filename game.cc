3dc88170dcc67ace8b89c2440146921d2c450657
ho_1041841 <1041841@my.scccd.edu>
2026-04-26 03:57:45 -0700

    fix: logic bugs

--- game.cc
+++ game.cc
@@ -172,7 +172,7 @@ Game::Game() {
 		start_color();
 		init_pair(1, COLOR_GREEN, COLOR_BLACK);
 		init_pair(2, COLOR_RED, COLOR_BLACK);
-		init_pair(3, COLOR_BLUE, COLOR_BLACK);
+		init_pair(3, COLOR_WHITE, COLOR_BLUE);
 		init_pair(4, COLOR_YELLOW, COLOR_BLACK);
 		init_pair(5, COLOR_WHITE, COLOR_BLACK);
 		init_pair(6, COLOR_WHITE, COLOR_BLUE);
